21b5a59d65b629b238575af71f7eb4b9c4606ad3
ben <79266160+bencula@users.noreply.github.com>
2021-06-11 11:57:43 +0100

    Updated World Layouts
    In Progress

--- Assets/TreeMoveHorizontal.cs
+++ Assets/TreeMoveHorizontal.cs
@@ -7,12 +7,17 @@ public class TreeMoveHorizontal : MonoBehaviour
 {
     [SerializeField] Vector2 destination;
     [SerializeField] float duration = 1;
+    [SerializeField] float timeBeforeStart = 1; // THIS LOlllllllllllllllllllllllllllllllllllll
+    [SerializeField] float timeBeforeRestart = 1; // THIS LOlllllllllllllllllllllllllllllllllllll
 
+  
     Vector2 startPos;
     RectTransform rectTransform;
     // Start is called before the first frame update
     void Start()
+
     {
+ 
         rectTransform = GetComponent<RectTransform>();
         startPos = rectTransform.anchoredPosition;
 
@@ -23,8 +28,11 @@ public class TreeMoveHorizontal : MonoBehaviour
     {
         while (true)
         {
+            yield return new WaitForSeconds(timeBeforeStart);
             yield return rectTransform.DOAnchorPos(destination, duration).WaitForCompletion();
+            yield return new WaitForSeconds(timeBeforeRestart); // THIS LOlllllllllllllllllllllllllllllllllllll
             rectTransform.anchoredPosition = startPos;
+           
         }
     }
 }
