8b487be36aa1b9c24e80ee7db939bf025f70761d
HardCrabS <slavysia02@gmail.com>
2021-08-05 13:56:31 +0300

    bad words list updated

--- Assets/UsernameCheck.cs
+++ Assets/UsernameCheck.cs
@@ -6,16 +6,20 @@ using UnityEngine.Events;
 
 public class UsernameCheck : MonoBehaviour
 {
-    [SerializeField] string[] badWords;
+    [SerializeField] TextAsset badWordTxt;
+    public TMP_InputField inputField;
 
     public UnityEvent onUsernameConfirm = new UnityEvent();
-
-    public TMP_InputField inputField;
+    
+    string[] badWords;
 
     private void Start()
     {
         inputField = GetComponentInChildren<TMP_InputField>();
         inputField.onEndEdit.AddListener(delegate { ConfirmUsername(inputField); });
+
+        char[] archDelim = new char[] { '\r', '\n' };
+        badWords = badWordTxt.text.Split(archDelim, System.StringSplitOptions.RemoveEmptyEntries);
     }
 
     void ConfirmUsername(TMP_InputField input)
@@ -40,8 +44,15 @@ public class UsernameCheck : MonoBehaviour
     {
         foreach (var word in badWords)
         {
-            if (username.ToLower().Contains(word))
+            string lowerUsername = username.ToLower();
+            if (lowerUsername.Contains(word))
+            {
+                //bad word is short and can be just a part of normal long word
+                if (word.Length == 3 && lowerUsername != word)
+                    return true;
+                //bad word
                 return false;
+            }
         }
         return true;
     }
