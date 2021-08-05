cd126f010cbd0608dba956997ac02b4ac3d2999a
ben <79266160+bencula@users.noreply.github.com>
2021-08-05 13:38:56 +0100

    Finalized Magic Monolith Boost

--- Assets/Scripts/Boosts/MagicMonolith.cs
+++ Assets/Scripts/Boosts/MagicMonolith.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class MagicMonolith : BoostBase
 {
     int blockToMakeSpecial = 3;
     GameObject magicBarrier;
+    GameObject monolith;
     AudioClip flashStart, flashStrike;
+    AudioClip monostart;
     GridA grid;
 
     public override void ExecuteBonus()
@@ -14,22 +17,38 @@ public class MagicMonolith : BoostBase
         {
             magicBarrier = Resources.Load<GameObject>(RESOURCES_FOLDER + "Magic Monolith/MagicBarrierBlast");
             flashStrike = Resources.Load<AudioClip>(RESOURCES_FOLDER + "Magic Monolith/sfx_boost_monolithnoise2");
+            monolith = Resources.Load<GameObject>(RESOURCES_FOLDER + "Magic Monolith/Obelisk_effects_0");
+            monostart = Resources.Load<AudioClip>(RESOURCES_FOLDER + "Magic Monolith/sfx_boost_mono1");
+           
         }
-        audioSource.PlayOneShot(flashStart);
+
         StartCoroutine(MakeAllSpecial());
+
     }
 
     IEnumerator MakeAllSpecial()
     {
+        audioSource.PlayOneShot(flashStart);
+        GameObject monolithgo = Instantiate(monolith, new Vector2(3.59f, -3.51f), transform.rotation); // spawn monolith
+        monolithgo.transform.DOMove(new Vector2(3.59f, 6.32f), 1); //move monolith to middle
+        audioSource.PlayOneShot(monostart); //play audio sfx
+
         yield return new WaitForSeconds(1f);
+        Camera.main.GetComponent<CameraShake>().ShakeCam(0.5f, 0.3f); //shake screen effect
+
         for (int i = 0; i < blockToMakeSpecial; i++) //Get number of Blocks to make Special
         {
+
             int randX = Random.Range(0, grid.width);
             int randY = Random.Range(0, grid.hight);
             if (grid.allBoxes[randX, randY] != null)
                 MakeBlockSpecial(grid.allBoxes[randX, randY].GetComponent<Box>(), new Vector2(randX, randY)); //Runs MakeBlockFiredUp Script below on random blocks
             yield return new WaitForSeconds(0.1f);
         }
+
+        monolithgo.GetComponent<SpriteRenderer>().DOFade(0, 0.3f); //FADE OUT MONOLITH HERE
+        Camera.main.GetComponent<CameraShake>().ShakeCam(1f, 0.3f); //shake screen effect
+
         finished = true;
     }
 
@@ -37,8 +56,8 @@ public class MagicMonolith : BoostBase
     {
         audioSource.PlayOneShot(flashStrike); //play audio sfx
         GameObject go = Instantiate(magicBarrier, pos, transform.rotation); // make magic barrier
-        Camera.main.GetComponent<CameraShake>().ShakeCam(0.1f, 1f); //shake screen effect
-        Destroy(go, 0.4f); //destroy instance of magic barrier
+        //Camera.main.GetComponent<CameraShake>().ShakeCam(0.5f, 0.3f); //shake screen effect
+        Destroy(go, 2f); //destroy instance of magic barrier
 
         int specialBlockChance = Random.Range(0, 100);
         if (specialBlockChance < 33)
