1b46f5a63e6bbf558ac60b4a36149b345e9e53a4
HardCrabS <slavysia02@gmail.com>
2021-08-25 14:11:00 +0300

    Fixed Retiring before blocks are spawned

--- Assets/Scripts/Managers/EndGameManager.cs
+++ Assets/Scripts/Managers/EndGameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public class EndGameManager : MonoBehaviour
 {
     [SerializeField] Text bestScoreText;
+    [SerializeField] GameObject counter321;
     [SerializeField] CanvasGroup visualizerCanvasGroup;
     [SerializeField] UnityEvent onGameOver;
 
@@ -35,6 +36,53 @@ public class EndGameManager : MonoBehaviour
             yield return new WaitForSeconds(0.31f);
             StartCoroutine(GridA.Instance.MoveBoxesDown());
         }
+    }
+
+    public void Retire()
+    {
+        if (gameIsOver) return;//avoid multiple calls of GameOver
+        StartCoroutine(RetireCo());
+        gameIsOver = true;
+    }
+
+    IEnumerator RetireCo()
+    {
+        AudioController.Instance.StartFade(2, 0);//fade out music
+        if (GridA.Instance.allBoxes != null)//blocks spawned
+        {
+            BonusManager.Instance.SetAllButtonsInterraction(false);//disable boosts
+            GridA.Instance.currState = GameState.wait;
+
+            //wait until boost stop executing
+            yield return new WaitUntil(() => !BonusManager.Instance.BoostIsActivated());
+            //if boost was activated, disable all boosts again
+            BonusManager.Instance.SetAllButtonsInterraction(false);
+            GridA.Instance.currState = GameState.wait;//set grid state again
+
+            //wait while blocks move and match
+            while (GridA.Instance.AcitivityOnBoard())
+            {
+                yield return new WaitForSeconds(.5f);
+            }
+            string worldId = LevelSettingsKeeper.settingsKeeper == null ? "Twilight City"
+                : LevelSettingsKeeper.settingsKeeper.worldLoadInfo.id;
+
+            PlayGamesController.PostToLeaderboard(worldId);
+            GridA.Instance.BlocksBlackAndWhite();
+            StartCoroutine(GridA.Instance.DeadlockMoveBoxesDown());
+        }
+        else
+        {
+            //disable start 3,2,1 counter
+            counter321.SetActive(false);
+            //stop block spawning Co
+            GridA.Instance.StopAllCoroutines();
+        }
+
+        LeaderboardController.Instance.SetLeaderboard();
+        onGameOver.Invoke();
+        StartCoroutine(CanvasGroupFadeOut(visualizerCanvasGroup, 1));
+        bestScoreText.text = Score.Instance.GetCurrentScore().ToString();
     }
 
     public void GameOver()
@@ -78,7 +126,7 @@ public class EndGameManager : MonoBehaviour
     IEnumerator CanvasGroupFadeOut(CanvasGroup canvasGroup, float duration)
     {
         float value = 0.1f / duration;
-        while(canvasGroup.alpha > 0)
+        while (canvasGroup.alpha > 0)
         {
             canvasGroup.alpha -= value;
             yield return new WaitForSeconds(value);
