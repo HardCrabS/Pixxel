8ffc7d54a5d298312541bb106ff00cd1fc428119
HardCrabS <slavysia02@gmail.com>
2021-08-28 22:12:56 +0300

    open url script

---
+++ Assets/urlButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class urlButton : MonoBehaviour
+{
+    [SerializeField] string url;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OpenURL);
+    }
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
+    }
+}
