b23bad504921b8b109a4be2767f738e634162368
HardCrabS <slavysia02@gmail.com>
2021-09-10 23:50:48 +0300

    Titles in score panels

--- Assets/High Score/HoldScorePanel.cs
+++ Assets/High Score/HoldScorePanel.cs
@@ -9,29 +9,16 @@ public class HoldScorePanel : MonoBehaviour
     [SerializeField] Image profileImage;
     [SerializeField] Image bannerImage;
     [SerializeField] Text titleText;
-    public GameObject title;
 
-    private void Start()
-    {
-        
-    }
-
-    public void onPress()
-    {
-        title.SetActive(true);
-    }
-
-    public void onRelease()
-    {
-        title.SetActive(false);
-    }
     public void SetScorePanel(string _name, Sprite _profileImage = null, string bannerPath = null, string _title = null)
     {
         nameText.text = _name;
         if (_profileImage != null)
             profileImage.sprite = _profileImage;
         if (!string.IsNullOrEmpty(_title))
+        {
             titleText.text = _title;
+        }
         if(!string.IsNullOrEmpty(bannerPath))
             ProfileHandler.SetBannerFromString(bannerImage, bannerPath);
     }
